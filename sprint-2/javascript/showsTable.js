6152e4dd163e6c51a8098841d76627b37b601d14
Codee Palmer <codeejpalmer@gmail.com>
2019-10-16 14:33:00 -0700

    table for mobile working-styling needed

--- sprint-2/javascript/showsTable.js
+++ sprint-2/javascript/showsTable.js
@@ -1,56 +1,74 @@
-var shows = [
+const shows = [
 	{
-		date: "Mon Dec 17 2018",
-		venue: "Ronald Lane",
-		location: "San Francisco, CA"
+		Date: "Mon Dec 17 2018",
+		Venue: "Ronald Lane",
+		Location: "San Francisco, CA"
 	},
 	{
-		date: "Tue Jul 18 2019",
-		venue: "Pier 3 East",
-		location: "San Francisco, CA"
+		Date: "Tue Jul 18 2019",
+		Venue: "Pier 3 East",
+		Location: "San Francisco, CA"
 	},
 	{
-		date: "Fri Jul 22 2019",
-		venue: "View Lounge",
-		location: "San Francisco, CA"
+		Date: "Fri Jul 22 2019",
+		Venue: "View Lounge",
+		Location: "San Francisco, CA"
 	},
 	{
-		date: "Sat Aug 12 2019",
-		venue: "Hyatt Agency",
-		location: "San Francisco, CA"
+		Date: "Sat Aug 12 2019",
+		Venue: "Hyatt Agency",
+		Location: "San Francisco, CA"
+	},
+
+	{
+		Date: "Fri Sep 05 2019",
+		Venue: "Moscow Center",
+		Location: "San Francisco, CA"
 	},
 	{
-		date: "Fri Sep 05 2019",
-		venue: "Moscow Center",
-		location: "San Francisco, CA"
-	},
-	{ date: "Wed Aug 11 2019", venue: "Pres Club", location: "San Francisco, CA" }
+		Date: "Wed Aug 11 2019",
+		Venue: "Pres Club",
+		Location: "San Francisco, CA"
+	}
 ];
+
 function createTable(table, shows) {
-	for (var show of shows) {
-		createTableHead(table, showKeys);
-		var row = table.insertRow();
+	for (const show of shows) {
+		// for...of only works on arrays
 		for (key in show) {
-			var cell = row.insertCell();
-			var text = document.createTextNode(show[key]);
+			let row = table.insertRow();
+			//create row
+			createTableHead(showKeys, row);
+			//call heading function
+			let cell = row.insertCell();
+			let text = document.createTextNode(show[key]);
 			cell.appendChild(text);
+			//creating a button after each iteration
+			if (count % 3 === 2) {
+				let row = table.insertRow();
+				let btnCell = row.insertCell();
+				let btn = document.createElement("button");
+				btn.innerHTML = "CLICK ME";
+				btnCell.appendChild(btn);
+			}
+			count++; //iterating through heading function
 		}
 	}
 }
 
-function createTableHead(table, shows) {
-	var tHead = table.createTHead();
-	var row = tHead.insertRow();
-	for (var show of shows) {
-		var td = document.createElement("td");
-		var text = document.createTextNode(show);
-		td.appendChild(text);
-		row.appendChild(td);
-	}
+function createTableHead(shows, row) {
+	let variable = count % 3;
+	// let tHead = table.createTHead();
+	// let row = tHead.insertRow();
+	let th = document.createElement("th");
+	let text = document.createTextNode(shows[variable]);
+	th.appendChild(text);
+	row.appendChild(th);
 }
-var table = document.querySelector("table");
-var showKeys = Object.keys(shows[0]);
+
+const table = document.querySelector("table");
+const showKeys = Object.keys(shows[0]);
+let count = 3;
 
 // function invocation
 createTable(table, shows); // actually pass the students array
-createTableHead(table, showKeys);
