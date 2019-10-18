e50429b20e3c8054201a6f1abc74805eab40abdb
CodeeCodes <codeejpalmer@gmail.com>
2019-10-18 13:21:44 -0700

    working on comments

--- sprint-2/javascript/commentsForm.js
+++ sprint-2/javascript/commentsForm.js
@@ -1,4 +1,4 @@
-const commentsObj = [
+let commentsObj = [
 	{
 		name: "Micheal Lyons",
 		comment:
@@ -15,31 +15,56 @@ const commentsObj = [
 			"How can someone be soo good!!! You can tell he lives for this and loves to do it everyday. Every time I see him I get instantly happy! He's definitely my favorite ever"
 	}
 ];
-console.log(commentsObj);
-
+// const commentsOriginal = document.createElement("div");
 const comments = document.getElementById("comments__section");
+// collecting the comments section from the document
 const newComments = document.getElementById("comments");
+// the empty div in the HTML
 const formValue = comments__section.getElementsByTagName('input["type = text"]')
 	.value; //getting the value from the <input> tag with type text
 
-comments.addEventListener("submit", function(event) {
+// creating a for loop to loop through each object and create textNode of values
+
+const commentKeysTwo = Object.keys(commentsObj[1]);
+const commentKeys = Object.keys(commentsObj[0]);
+for (i = 0; i < commentsObj.length; i++) {
+	let text = document.createTextNode(commentsObj[i].name);
+	let textOne = document.createTextNode(commentsObj[i].comment);
+	newComments.appendChild(text);
+	newComments.appendChild(textOne);
+	// newComments.appendChild(commentsOriginal);
+}
+
+comments.addEventListener("submit", (event) => {
+	// callback function so it can do the function once submit is submitted
 	event.preventDefault();
+
+	let name = event.target.name.value;
+	let comment = event.target.comment.value;
+	//pulling info from form
 
 	//create element
 	// const commentAvatar = document.createElement("img")
 
-	const inputText = document.createElement("p");
-	const createName = document.createElement("h3");
-	const commentsDivSmall = document.createElement("div");
+	let inputText = document.createElement("p");
+	let createName = document.createElement("h3");
+	let commentsDivSmall = document.createElement("div");
 
 	// append to DOM
 
 	// createName.innerHTML = formValue; //using the createName variable .innerHTML to equal the value
 	// of formValue which is pulling data from the form
-	inputText.innerHTML = formValue;
+	let commentsName = document.createTextNode(name);
+	let commentsText = document.createTextNode(comment);
+
+	// createName.innerText = commentsName;
+	// inputText.innerText = commentsText;
+
+	createName.appendChild(commentsName);
+	inputText.appendChild(commentsText);
+
 	commentsDivSmall.appendChild(createName);
 	commentsDivSmall.appendChild(inputText);
-	comments.appendChild(commentsDivSmall);
-
-	console.log(newComments);
+	newComments.appendChild(commentsDivSmall);
 });
+console.log(newComments);
