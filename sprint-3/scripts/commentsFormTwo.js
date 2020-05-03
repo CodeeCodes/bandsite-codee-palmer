8a57c6e62a9c0d18f533e7aae7269bf70bc7a745
Codee J Palmer <codeecodes92@gmail.com>
2020-05-03 15:57:02 -0700

    Looking over old code, looking to improve

--- sprint-3/scripts/commentsFormTwo.js
+++ sprint-3/scripts/commentsFormTwo.js
@@ -1,16 +1,16 @@
 let count = 3;
 
-//Axios
+//Axios to retrieve comments
 
 function retrieveComments() {
-	axios
-		.get(
-			"https://project-1-api.herokuapp.com/comments?api_key=3a2efde3-55a8-4d11-8445-f9f2d74b6929"
-		)
-		.then((response) => {
-			// console.log(response);
-			createObject(response.data);
-		});
+  axios
+    .get(
+      "https://project-1-api.herokuapp.com/comments?api_key=3a2efde3-55a8-4d11-8445-f9f2d74b6929"
+    )
+    .then((response) => {
+      // console.log(response);
+      createObject(response.data);
+    });
 }
 
 //code to get comments to go into the div created in HTML------------------------
@@ -21,90 +21,91 @@ const comments = document.getElementById("comments__section");
 const newComments = document.getElementById("comments");
 //count for createObject function
 
+//creating object to pass through
 function createObject(commentsObj) {
-	const deletedDiv = document.createElement("div");
-	deletedDiv.setAttribute("id", "deleted-div");
-	newComments.appendChild(deletedDiv);
-	//while count is less than length of comments object length
-	for (let i = commentsObj.length - 1; i >= 0; i--) {
-		// deletedDiv.appendChild(newComments);
-		let newCommentsAvatar = document.createElement("img");
-		newCommentsAvatar.src = "sprint-3/assets/Images/avatar.jpg";
-		newCommentsAvatar.className = "new-comments-avatar";
-		let newCommentsDiv = document.createElement("div");
-		newCommentsDiv.className = "new-comments-div";
-		let originalHeading = document.createElement("h3");
-		originalHeading.className = "new-comments-name";
-		let originalParagraph = document.createElement("p");
-		originalParagraph.className = "new-comments-paragraph";
-		let originalDate = document.createElement("div");
-		originalDate.className = "new-comments-date";
+  const deletedDiv = document.createElement("div");
+  deletedDiv.setAttribute("id", "deleted-div");
+  newComments.appendChild(deletedDiv);
+  //while count is less than length of comments object length
+  for (let i = commentsObj.length - 1; i >= 0; i--) {
+    // deletedDiv.appendChild(newComments);
+    let newCommentsAvatar = document.createElement("img");
+    newCommentsAvatar.src = "sprint-3/assets/Images/avatar.jpg";
+    newCommentsAvatar.className = "new-comments-avatar";
+    let newCommentsDiv = document.createElement("div");
+    newCommentsDiv.className = "new-comments-div";
+    let originalHeading = document.createElement("h3");
+    originalHeading.className = "new-comments-name";
+    let originalParagraph = document.createElement("p");
+    originalParagraph.className = "new-comments-paragraph";
+    let originalDate = document.createElement("div");
+    originalDate.className = "new-comments-date";
 
-		// creating a for loop to loop through each object and create textNode of values
-		//create a avatar
-		newCommentsDiv.appendChild(newCommentsAvatar);
-		//pulling information to make Persons name textnode then appending it to child (h3) of newCommentsDiv
-		let text = document.createTextNode(commentsObj[i].name);
-		originalHeading.appendChild(text);
-		newCommentsDiv.appendChild(originalHeading);
-		//pulling information to make date textnode then appending it to child of newCommentsDiv
-		let date = commentsObj[i].timestamp;
-		let today = new Date();
-		date =
-			today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
-		date = document.createTextNode(date);
-		originalDate.appendChild(date);
-		newCommentsDiv.appendChild(originalDate);
-		//pulling information to make comment textnode then appending it to child <p> of newCommentsDiv
-		let textOne = document.createTextNode(commentsObj[i].comment);
-		originalParagraph.appendChild(textOne);
-		newCommentsDiv.appendChild(originalParagraph);
-		// appending all elements to the empty div tag in html
-		deletedDiv.appendChild(newCommentsDiv);
-	}
+    // creating a for loop to loop through each object and create textNode of values
+    //create a avatar
+    newCommentsDiv.appendChild(newCommentsAvatar);
+    //pulling information to make Persons name textnode then appending it to child (h3) of newCommentsDiv
+    let text = document.createTextNode(commentsObj[i].name);
+    originalHeading.appendChild(text);
+    newCommentsDiv.appendChild(originalHeading);
+    //pulling information to make date textnode then appending it to child of newCommentsDiv
+    let date = commentsObj[i].timestamp;
+    let today = new Date();
+    date =
+      today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
+    date = document.createTextNode(date);
+    originalDate.appendChild(date);
+    newCommentsDiv.appendChild(originalDate);
+    //pulling information to make comment textnode then appending it to child <p> of newCommentsDiv
+    let textOne = document.createTextNode(commentsObj[i].comment);
+    originalParagraph.appendChild(textOne);
+    newCommentsDiv.appendChild(originalParagraph);
+    // appending all elements to the empty div tag in html
+    deletedDiv.appendChild(newCommentsDiv);
+  }
 }
 
 //---------------------------------------------------------------------------------------
 
 comments.addEventListener("submit", (event) => {
-	// callback function so it can do the function once submit is submitted
-	//prevent default behaviour of the form element (page refresh)
-	event.preventDefault();
+  // callback function so it can do the function once submit is submitted
+  //prevent default behaviour of the form element (page refresh)
+  event.preventDefault();
 
-	//pulling information from event <form>  and assigning them to variables
-	let name = event.target.name.value;
-	let comment = event.target.comment.value;
+  //pulling information from event <form>  and assigning them to variables
+  let name = event.target.name.value;
+  let comment = event.target.comment.value;
 
-	let axiosObject = {
-		name: name,
-		comment: comment
-		// timestamp: date
-	};
+  let axiosObject = {
+    name: name,
+    comment: comment,
+    // timestamp: date
+  };
 
-	axios
-		.post(
-			"https://project-1-api.herokuapp.com/comments?api_key=3a2efde3-55a8-4d11-8445-f9f2d74b6929",
-			axiosObject
-		)
-		.then(function(response) {
-			removeDiv();
-			console.log(response);
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-	setTimeout(function() {
-		retrieveComments();
-	}, 250);
+  axios
+    .post(
+      "https://project-1-api.herokuapp.com/comments?api_key=3a2efde3-55a8-4d11-8445-f9f2d74b6929",
+      axiosObject
+    )
+    .then(function (response) {
+      removeDiv();
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  setTimeout(function () {
+    retrieveComments();
+  }, 250);
 
-	// retrieveComments();
-	comments.reset();
+  // retrieveComments();
+  comments.reset();
 });
 // function removeDiv() {
 // 	newComments.removeChild(newComments.childNodes[0]);
 // }
 function removeDiv() {
-	let del = document.getElementById("comments");
-	del.removeChild(del.childNodes[0]);
+  let del = document.getElementById("comments");
+  del.removeChild(del.childNodes[0]);
 }
 retrieveComments();
--- sprint-3/scripts/showsTableTwo.js
+++ sprint-3/scripts/showsTableTwo.js
@@ -1,63 +1,75 @@
 const URL =
-	"https://project-1-api.herokuapp.com/showdates?api_key=49124594-6b32-4c22-8ce7-d4004cd062f5";
+  "https://project-1-api.herokuapp.com/showdates?api_key=49124594-6b32-4c22-8ce7-d4004cd062f5";
 
 axios.get(URL).then((response) => {
-	// console.log(response.data);
-	createTableDivs(response.data);
+  //   console.log(response.data);
+  createTableDivs(response.data);
 });
 let count = 0;
 
 const newShows = document.querySelector(".shows-section");
 
 function createTableDivs(tableDivs) {
-	//while count is less than length of comments object length
-	while (count < tableDivs.length) {
-		let newShowDiv = document.createElement("div");
-		newShowDiv.className = "new__show-div";
-		let newHeading = document.createElement("h2");
-		newHeading.className = "new__show-heading";
-		let newShowDate = document.createElement("h4");
-		newShowDate.className = "new__show-date";
-		let newShowVenue = document.createElement("h5");
-		newShowVenue.className = "new__show-Venue";
-		let newShowLocation = document.createElement("h5");
-		newShowLocation.className = "new__show-location";
-		let newShowButton = document.createElement("button");
-		newShowButton.innerHTML = "BUY TICKETS";
-		newShowButton.className = "new__show-button";
-		let dateKey = document.createElement("h5");
-		dateKey.innerHTML = "DATE";
-		dateKey.className = "hidden-key";
-		let venueKey = document.createElement("h5");
-		venueKey.innerHTML = "VENUE";
-		venueKey.className = "hidden-key";
-		let locationKey = document.createElement("h5");
-		locationKey.innerHTML = "LOCATION";
-		locationKey.className = "hidden-key";
+  //while count is less than length of comments object length
+  while (count < tableDivs.length) {
+    let newShowDiv = document.createElement("div");
+    newShowDiv.className = "new__show-div";
 
-		// creating a for loop to loop through each object and create textNode of values
-		//create a avatar
-		newShowDiv.appendChild(newHeading);
-		//pulling information to make Persons name textnode then appending it to child (h3) of newCommentsDiv
-		newShowDiv.appendChild(dateKey);
-		//pulling information to make date textnode then appending it to child of newCommentsDiv
-		let date = document.createTextNode(tableDivs[count].date);
-		newShowDate.appendChild(date);
-		newShowDiv.appendChild(newShowDate);
-		//pulling information to make comment textnode then appending it to child <p> of newCommentsDiv
-		newShowDiv.appendChild(venueKey);
-		let venue = document.createTextNode(tableDivs[count].place);
-		newShowVenue.appendChild(venue);
-		newShowDiv.appendChild(newShowVenue);
-		//
-		newShowDiv.appendChild(locationKey);
-		let location = document.createTextNode(tableDivs[count].location);
-		newShowLocation.appendChild(location);
-		newShowDiv.appendChild(newShowLocation);
+    let newHeading = document.createElement("h2");
+    newHeading.className = "new__show-heading";
 
-		// appending all elements to the empty div tag in html
-		newShowDiv.appendChild(newShowButton);
-		newShows.appendChild(newShowDiv);
-		count++;
-	}
+    let newShowDate = document.createElement("h4");
+    newShowDate.className = "new__show-date";
+
+    let newShowVenue = document.createElement("h5");
+    newShowVenue.className = "new__show-Venue";
+
+    let newShowLocation = document.createElement("h5");
+    newShowLocation.className = "new__show-location";
+
+    let newShowButton = document.createElement("button");
+    newShowButton.innerHTML = "BUY TICKETS";
+    newShowButton.className = "new__show-button";
+
+    let dateKey = document.createElement("h5");
+    dateKey.innerHTML = "DATE";
+    dateKey.className = "hidden-key";
+
+    let venueKey = document.createElement("h5");
+    venueKey.innerHTML = "VENUE";
+    venueKey.className = "hidden-key";
+
+    let locationKey = document.createElement("h5");
+    locationKey.innerHTML = "LOCATION";
+    locationKey.className = "hidden-key";
+
+    // creating a for loop to loop through each object and create textNode of values
+    //create a avatar
+    newShowDiv.appendChild(newHeading);
+
+    //pulling information to make Persons name textnode then appending it to child (h3) of newCommentsDiv
+    newShowDiv.appendChild(dateKey);
+
+    //pulling information to make date textnode then appending it to child of newCommentsDiv
+    let date = document.createTextNode(tableDivs[count].date);
+    newShowDate.appendChild(date);
+    newShowDiv.appendChild(newShowDate);
+
+    //pulling information to make comment textnode then appending it to child <p> of newCommentsDiv
+    newShowDiv.appendChild(venueKey);
+    let venue = document.createTextNode(tableDivs[count].place);
+    newShowVenue.appendChild(venue);
+    newShowDiv.appendChild(newShowVenue);
+
+    //
+    newShowDiv.appendChild(locationKey);
+    let location = document.createTextNode(tableDivs[count].location);
+    newShowLocation.appendChild(location);
+    newShowDiv.appendChild(newShowLocation);
+
+    // appending all elements to the empty div tag in html
+    newShowDiv.appendChild(newShowButton);
+    newShows.appendChild(newShowDiv);
+    count++;
+  }
 }
